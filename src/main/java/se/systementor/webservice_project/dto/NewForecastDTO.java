e06f9cb6c3757f8cdf6fa0c8c70dc730fdc8ea5d
Kevin Åhlin <kevin.ahlin@hotmail.com>
2023-09-05 15:38:35 +0200

    added a DTO

---
+++ src/main/java/se/systementor/webservice_project/dto/NewForecastDTO.java
@@ -0,0 +1,14 @@
+package se.systementor.webservice_project.dto;
+
+public class NewForecastDTO {   // Data Transfer Object - 'box' with properties
+
+    public int Date; // 20230101
+    public int Hour;
+    public float Temperature;
+
+
+
+
+
+
+}
